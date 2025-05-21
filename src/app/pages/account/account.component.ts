c95944d5894c83eff337defc06f1cc1a2ccfe85a
Felix ASTO <felix_ab2@hotmail.com>
2025-05-21 12:14:43 -0500

    feat: login data

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -3,6 +3,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { SecurityPrivacityComponent } from './security-privacity/security-privacity.component';
 import { ChangePasswordComponent } from '../../components/change-password/change-password.component';
 import { VerificAccountComponent } from '../../components/verific-account/verific-account.component';
+import { LoginComponent } from '../../shared/login/login.component';
 
 @Component({
   selector: 'tyn-account',
@@ -11,6 +12,7 @@ import { VerificAccountComponent } from '../../components/verific-account/verifi
     SecurityPrivacityComponent,
     ChangePasswordComponent,
     VerificAccountComponent,
+    LoginComponent,
   ],
   templateUrl: './account.component.html',
 })
---
+++ src/app/shared/login/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tyn-login',
+  imports: [],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent { }
