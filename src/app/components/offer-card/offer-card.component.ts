b0f0db56c1e6785e0d3f34b264cf408ee82d5170
Flor Tinco <flor.tinco.27@unsch.edu.pe>
2025-05-23 16:59:14 -0500

    feat: modal offert

--- src/app/components/offer-card/offer-card.component.ts
+++ src/app/components/offer-card/offer-card.component.ts
@@ -1,10 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { CardOffer } from '@app/interfaces/card.interface';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'tyn-offer-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './offer-card.component.html',
 })
 export class OfferCardComponent {
   listOffer = input.required<CardOffer[]>();
+   isModalOpen = signal(false);
+
+  openModal() {
+    this.isModalOpen.set(true);
+  }
+  closeModal() {
+    this.isModalOpen.set(false);
+  }
 }
