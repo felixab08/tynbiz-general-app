0ff9c9d9578b3631390089af74a3809c0a6b6698
Felix ASTO <felix_ab2@hotmail.com>
2025-05-23 13:34:30 -0500

    feat: update login

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,14 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '@app/utils/form.util';
 
 @Component({
   selector: 'tyn-login',
-  imports: [CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
   isModalOpen = signal(false);
+  private _fb = inject(FormBuilder);
 
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this._fb.group({
+    user: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log('Form submitted', this.myForm.value);
+    this.myForm.reset();
+  }
   openModal() {
     this.isModalOpen.set(true);
   }
