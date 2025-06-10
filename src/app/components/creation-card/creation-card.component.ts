b6a466aac8baaa54b652553538cf08a1b846133f
Flor Tinco <flor.tinco.27@unsch.edu.pe>
2025-06-10 17:57:54 -0500

    feat: carouselProduct

---
+++ src/app/components/creation-card/creation-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, input, signal } from '@angular/core';
+import { Cardcreations } from '@app/interfaces/card.interface';
+import { CommonModule } from '@angular/common';
+import { initCarousels } from 'flowbite';
+@Component({
+  selector: 'tyn-creation-card',
+  imports: [CommonModule],
+  templateUrl: './creation-card.component.html',
+})
+export class CreationCardComponent {
+  listCreation = input.required<Cardcreations[]>();
+  isModalOpen = signal(false);
+  selectedcreations: any = true;
+  ngAfterViewInit(): void {
+    initCarousels(); // inicializa el carrusel de Flowbite
+  }
+  openModal(creations: any) {
+    this.selectedcreations = creations;
+    this.isModalOpen.set(true);
+  }
+  closeModal() {
+    this.isModalOpen.set(false);
+  }
+}
