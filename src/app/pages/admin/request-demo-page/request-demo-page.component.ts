015695bd1c0405bc11e26fe3ccf0f5b91688a115
Felix ASTO <felix_ab2@hotmail.com>
2025-06-11 23:25:43 -0500

    feat: table create

--- src/app/pages/admin/request-demo-page/request-demo-page.component.ts
+++ src/app/pages/admin/request-demo-page/request-demo-page.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { resquestDemoListMock } from '../../../mock/resquet-demo-list.mock';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tyn-request-demo-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './request-demo-page.component.html',
 })
-export default class RequestDemoPageComponent {}
+export default class RequestDemoPageComponent {
+  resquestList = resquestDemoListMock;
+  lang = 'All'; // Default language set to Spanish
+  constructor() {
+    let data = this.filterResquestList('Pendiente');
+    console.log('Felix:::::>');
+    console.log(data);
+
+    // Initialization logic can go here if needed
+  }
+  filterResquestList(status: string) {
+    return this.resquestList.filter((item) => item.storeStatus === status);
+  }
+  myFuncEnglish2(data: any) {
+    console.log('Felix:::::>', data);
+    // console.log(this.lang);
+  }
+}
