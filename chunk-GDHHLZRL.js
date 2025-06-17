b8e3523b18a8233af9d3f3bbd87e3b108b509e29
felixab08 <felix_ab2@hotmail.com>
2025-06-17 04:40:39 +0000

    Deploying to gh-pages from @ felixab08/tynbiz-general-app@9235a58eea6f5f4c7120ff8f1815cb8d74a82828 🚀

---
+++ chunk-GDHHLZRL.js
@@ -0,0 +1,2 @@
+import{Ba as p,Ua as s,Va as i,db as n}from"./chunk-HEU666SH.js";import"./chunk-2VMXMS7J.js";var t=class a{static \u0275fac=function(e){return new(e||a)};static \u0275cmp=p({type:a,selectors:[["tyn-plan-suscrip-page"]],decls:3,vars:0,template:function(e,c){e&1&&(s(0,"p"),n(1,"plan-suscrip-page works!"),i(),n(2,`,
+`))},encapsulation:2})};export{t as default};
