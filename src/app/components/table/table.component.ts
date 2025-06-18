c3ff39fad9e107ded8600237fd1d12a2be44e071
Felix ASTO <felix_ab2@hotmail.com>
2025-06-17 20:42:46 -0500

    feat agree value

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { resquestDemoListMock } from '@app/mock/resquet-demo-list.mock';
 import { userConectadosMock } from '@app/mock/rol.mock';
 
 @Component({
@@ -8,8 +9,14 @@ import { userConectadosMock } from '@app/mock/rol.mock';
   templateUrl: './table.component.html',
 })
 export class TableComponent {
-  @Input() dataSource: any[] = [...userConectadosMock];
-  headerTable: any[] = [];
+  @Input() dataSource: any[] = [...resquestDemoListMock];
+  headerTable: any[] = [
+    'storeName',
+    'storeSite',
+    'requestDate',
+    'storeEmail',
+    'storeStatusService',
+  ];
   @Input() displayedColumns: string[] = [
     'N°',
     'Usuario',
@@ -20,8 +27,9 @@ export class TableComponent {
     'Acciones',
   ];
   constructor() {
-    this.headerTable = this.dataSource.map((objeto: any) =>
-      Object.keys(objeto)
-    )[0];
+    // this.headerTable = this.dataSource.map((objeto: any) =>
+    //   Object.keys(objeto)
+    // )[0];
+    console.log('Felix:::::>', this.headerTable);
   }
 }
