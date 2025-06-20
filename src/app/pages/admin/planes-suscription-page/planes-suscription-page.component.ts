0e6c09e39a945b4e9149f5bbd3b886c7ca7266be
Flor Andrea Tinco Quispe <69224368+TincoF@users.noreply.github.com>
2025-06-19 19:04:28 -0500

    feat: modal new plan

--- src/app/pages/admin/planes-suscription-page/planes-suscription-page.component.ts
+++ src/app/pages/admin/planes-suscription-page/planes-suscription-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { plan } from '@app/mock/plan.mock';
 
 @Component({
@@ -9,4 +9,32 @@ import { plan } from '@app/mock/plan.mock';
 })
 export default class PlanesSuscriptionPageComponent {
   planList = plan;
+  selectedPlan: any = true;
+
+  openDropdownIndex: number | null = null;
+  isModalOpen =  signal(false)
+  isModalWarOpen = signal(false);
+
+  selectedTypePlan: string = '';
+  toggleDropdown(plan: any) {
+    this.selectedPlan = plan;
+    this.openDropdownIndex = this.openDropdownIndex === plan.id ? null : plan.id;
+
+  }
+  openModalWar() {
+    this.isModalWarOpen.set(true);
+    this.openDropdownIndex = null;
+  }
+  closeModalWar() {
+    this.isModalWarOpen.set(false);
+  }
+  openModal(){
+    this.isModalOpen.set(true)
+  }
+  closeModal(){
+    this.isModalOpen.set(false)
+  }
+
+
+
 }
