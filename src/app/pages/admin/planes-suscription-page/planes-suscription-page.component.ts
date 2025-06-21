cbe300b26c6813ddba9d345b1985b55c1777c95b
Flor Andrea Tinco Quispe <69224368+TincoF@users.noreply.github.com>
2025-06-21 10:25:57 -0500

    feat: modalNewPlan

--- src/app/pages/admin/planes-suscription-page/planes-suscription-page.component.ts
+++ src/app/pages/admin/planes-suscription-page/planes-suscription-page.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { plan } from '@app/mock/plan.mock';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tyn-planes-suscription-page',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './planes-suscription-page.component.html',
 })
 export default class PlanesSuscriptionPageComponent {
@@ -15,7 +16,13 @@ export default class PlanesSuscriptionPageComponent {
   isModalOpen =  signal(false)
   isModalWarOpen = signal(false);
 
-  selectedTypePlan: string = '';
+  selectedTypePlan: string = 'uso de Tynbiz';
+
+  currentPage = 1;
+  itemsPerPage = 10;
+
+  isOffer: boolean = true;
+
   toggleDropdown(plan: any) {
     this.selectedPlan = plan;
     this.openDropdownIndex = this.openDropdownIndex === plan.id ? null : plan.id;
@@ -33,6 +40,31 @@ export default class PlanesSuscriptionPageComponent {
   }
   closeModal(){
     this.isModalOpen.set(false)
+  }
+   totalPages() {
+    return Math.ceil(this.planList.length / this.itemsPerPage);
+  }
+  get paginatedData() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    return this.planList.slice(start, this.currentPage * this.itemsPerPage);
+  }
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages()) {
+      this.currentPage = page;
+    }
+  }
+  setPage(page: number) {
+    this.currentPage = page;
+  }
+   prevPage() {
+    if (this.currentPage > 1) this.currentPage--;
+  }
+  nextPage() {
+    if (this.currentPage < this.totalPages()) this.currentPage++;
+  }
+  onItemsPerPageChange(value: number) {
+    this.itemsPerPage = value;
+    this.currentPage = 1;
   }
 
 
