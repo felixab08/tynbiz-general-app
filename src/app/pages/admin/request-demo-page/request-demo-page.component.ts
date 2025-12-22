2c3c2b89efb3b517049438e8985b5c00ea22c520
Felix <gatoab2@gmail.com>
2025-12-22 18:08:48 -0500

    feat: delete request demo

--- src/app/pages/admin/request-demo-page/request-demo-page.component.ts
+++ src/app/pages/admin/request-demo-page/request-demo-page.component.ts
@@ -22,12 +22,6 @@ export default class RequestDemoPageComponent {
   isState = 'All';
   isModalOpen = signal(false);
   selectedSolicDemo: RequestDemoContent | null = null;
-  currentPage = 1;
-  itemsPerPage = 10;
-  search = '';
-  startDate: string = '';
-  endDate: string = '';
-  selectedTab: string = 'verifyInformation';
 
   // Filtros
   filterMenu = signal({
@@ -75,15 +69,6 @@ export default class RequestDemoPageComponent {
     });
   }
 
-  filterByStatus(status: string): void {
-    const isAll = status === 'All';
-    const filteredList = isAll
-      ? resquestDemoListMock
-      : resquestDemoListMock.filter((store) => store.storeStatus === status);
-    this.resquestList = [...filteredList];
-    this.currentPage = 1;
-  }
-
   openModal(SolicDemo: RequestDemoContent) {
     this.searchDemoById(SolicDemo.id);
     this.isModalOpen.set(true);
@@ -92,8 +77,6 @@ export default class RequestDemoPageComponent {
   searchDemoById(id: number) {
     this._requesDemoService.getRequestDemoById(id).subscribe({
       next: (resp: any) => {
-        console.log('resp------>');
-        console.log(resp);
         this.selectedSolicDemo = resp;
       },
       error: (error: any) => {
@@ -104,56 +87,5 @@ export default class RequestDemoPageComponent {
 
   closeModal() {
     this.isModalOpen.set(false);
-  }
-
-  get filteredData() {
-    return this.resquestList.filter((item) => {
-      const matchesSearch = item.storeName
-        .toLowerCase()
-        .includes(this.search.toLowerCase());
-
-      const itemDate = new Date(item.requestDate);
-      const start = this.startDate ? new Date(this.startDate) : null;
-      const end = this.endDate ? new Date(this.endDate) : null;
-      const matchesDate =
-        (!start || itemDate >= start) && (!end || itemDate <= end);
-      return matchesSearch && matchesDate;
-    });
-  }
-
-  get paginatedData() {
-    const start = (this.currentPage - 1) * this.itemsPerPage;
-    return this.filteredData.slice(start, this.currentPage * this.itemsPerPage);
-  }
-
-  get pagesArray(): number[] {
-    return Array.from({ length: this.totalPages() }, (_, i) => i + 1);
-  }
-
-  totalPages() {
-    return Math.ceil(this.filteredData.length / this.itemsPerPage);
-  }
-
-  goToPage(page: number) {
-    if (page >= 1 && page <= this.totalPages()) {
-      this.currentPage = page;
-    }
-  }
-
-  setPage(page: number) {
-    this.currentPage = page;
-  }
-
-  prevPage() {
-    if (this.currentPage > 1) this.currentPage--;
-  }
-
-  nextPage() {
-    if (this.currentPage < this.totalPages()) this.currentPage++;
-  }
-
-  onItemsPerPageChange(value: number) {
-    this.itemsPerPage = value;
-    this.currentPage = 1;
   }
 }
