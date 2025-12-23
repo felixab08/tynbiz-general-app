6467242146d8313521c0da896ccff37bf3a4c5b5
Felix <gatoab2@gmail.com>
2025-12-23 10:28:08 -0500

    feat: delete lista dasht

--- src/app/pages/admin/list-store-page/list-store-page.component.ts
+++ src/app/pages/admin/list-store-page/list-store-page.component.ts
@@ -27,13 +27,7 @@ export default class ListStorePageComponent {
   _router = inject(Router);
 
   isState = 'All';
-  // paginacion
-  currentPage = 1;
-  itemsPerPage = 10;
   // Filtros por fecha
-  search = '';
-  startDate: string = '';
-  endDate: string = '';
   isReportStore = [
     {
       id: 1111,
@@ -109,48 +103,5 @@ export default class ListStorePageComponent {
       queryParams: { status: state, page: 1, size: 5 },
       queryParamsHandling: 'merge',
     });
-  }
-
-  filterByStatus(status: string): void {
-    const isAll = status === 'All';
-    const filteList = isAll
-      ? resquestDemoListMock
-      : resquestDemoListMock.filter((store) => store.status === status);
-    this.rolLists = [...filteList];
-  }
-  get filteredData() {
-    return this.rolLists.filter((item) => {
-      const matchesSearch = item.storeName
-        .toLowerCase()
-        .includes(this.search.toLowerCase());
-
-      const itemDate = new Date(item.planDate);
-      const start = this.startDate ? new Date(this.startDate) : null;
-      const end = this.endDate ? new Date(this.endDate) : null;
-      const matchesDate =
-        (!start || itemDate >= start) && (!end || itemDate <= end);
-      return matchesSearch && matchesDate;
-    });
-  }
-  get paginatedData() {
-    const start = (this.currentPage - 1) * this.itemsPerPage;
-    return this.filteredData.slice(start, start + this.itemsPerPage);
-  }
-  totalPages() {
-    return Math.ceil(this.filteredData.length / this.itemsPerPage);
-  }
-  goToPage(page: number) {
-    if (page >= 1 && page <= this.totalPages()) {
-      this.currentPage = page;
-    }
-  }
-  setPage(page: number) {
-    this.currentPage = page;
-  }
-  prevPage() {
-    if (this.currentPage > 1) this.currentPage--;
-  }
-  nextPage() {
-    if (this.currentPage < this.totalPages()) this.currentPage++;
   }
 }
