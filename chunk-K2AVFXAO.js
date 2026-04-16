dddd922baa8c415f9a6522a6ba3dc5aea3ca3752
felixab08 <felix_ab2@hotmail.com>
2026-04-16 02:54:55 +0000

    Deploying to gh-pages from @ felixab08/tynbiz-general-app@108261f3cbe5dbd27a8c34056c858556eb6a7682 🚀

--- chunk-K2AVFXAO.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./chunk-DBI76LIG.js";import{Ga as n}from"./chunk-7F27GKRL.js";var a=t.baseUrl,i=class e{transform(r){return r||"./assets/img/no-image.jpg"}static \u0275fac=function(s){return new(s||e)};static \u0275pipe=n({name:"notImage",type:e,pure:!0})};export{i as a};
