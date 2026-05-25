429ea03016919eb36753cb7f55c6648935b6caa6
Felix <gatoab2@gmail.com>
2026-05-25 11:46:51 -0500

    feat: jitsi initial

---
+++ src/app/services/stores/jitsi.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IJitsiResp } from '@app/interfaces/stores/jitsi.interface';
+import { environment } from '@environments/environment.development';
+import { Observable } from 'rxjs';
+const baseUrl = environment.baseUrl;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JitsiService {
+  private _http = inject(HttpClient);
+
+  getJitsiStatus(idContenido: number): Observable<IJitsiResp> {
+    return this._http.get<IJitsiResp>(`${baseUrl}/jitsi/token/${idContenido}`);
+  }
+}
