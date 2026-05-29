824b1f0e159fa51e5f4b0d864a346d5edda844dc
Felix <gatoab2@gmail.com>
2026-05-29 09:59:51 -0500

    hotfix: jitsi error solition

--- src/app/components/jitsi/jitsi.component.ts
+++ src/app/components/jitsi/jitsi.component.ts
@@ -4,9 +4,9 @@ import {
   input,
   signal,
   ElementRef,
-  ViewChild,
+  viewChild,
+  effect,
   OnInit,
-  AfterViewInit,
   OnDestroy,
 } from '@angular/core';
 import { IErrorGeneralResp, IJitsiResp } from '@app/interfaces';
@@ -28,25 +28,28 @@ import { environment } from '@environments/environment';
     `,
   ],
 })
-export class JitsiComponent implements OnInit, AfterViewInit, OnDestroy {
+export class JitsiComponent implements OnInit, OnDestroy {
   private _jitsiSrv = inject(JitsiService);
   private _alertSrv = inject(AlertService);
 
   idContenido = input.required<number>();
   tokenJitsi = signal<IJitsiResp | null>(null);
 
-  @ViewChild('jitsiContainer', { static: false }) jitsiContainer!: ElementRef;
+  jitsiContainer = viewChild<ElementRef>('jitsiContainer');
   private api: any;
+
+  constructor() {
+    effect(() => {
+      const container = this.jitsiContainer();
+      const token = this.tokenJitsi();
+      if (container && token) {
+        this.initializeJitsi(token, container);
+      }
+    });
+  }
 
   ngOnInit() {
     this.getJitsiToken();
-  }
-
-  ngAfterViewInit() {
-    const currentToken = this.tokenJitsi();
-    if (currentToken) {
-      this.initializeJitsi(currentToken);
-    }
   }
 
   getJitsiToken() {
@@ -54,9 +57,6 @@ export class JitsiComponent implements OnInit, AfterViewInit, OnDestroy {
       next: (response) => {
         console.log(response);
         this.tokenJitsi.set(response);
-        if (this.jitsiContainer) {
-          this.initializeJitsi(response);
-        }
       },
       error: (error: IErrorGeneralResp) => {
         this._alertSrv.getAlert(
@@ -93,7 +93,7 @@ export class JitsiComponent implements OnInit, AfterViewInit, OnDestroy {
     });
   }
 
-  private async initializeJitsi(data: IJitsiResp) {
+  private async initializeJitsi(data: IJitsiResp, container: ElementRef) {
     if (this.api) {
       this.api.dispose();
     }
@@ -104,7 +104,7 @@ export class JitsiComponent implements OnInit, AfterViewInit, OnDestroy {
       const domain = environment.JITSI_URL.replace(/^https?:\/\//, '');
       const options = {
         roomName: data.roomName,
-        parentNode: this.jitsiContainer.nativeElement,
+        parentNode: container.nativeElement,
         jwt: data.token,
         width: '100%',
         height: '100%',
