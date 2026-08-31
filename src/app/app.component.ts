961a99191fd7f396526e650fa45d29e3c5e01b97
Felix <gatoab2@gmail.com>
2026-08-31 17:34:53 -0500

    feat: css styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  inject,
+} from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { SideMenuComponent } from './shared/side-menu/side-menu.component';
+import { StoreService } from './services';
+import { User } from './auth/interfaces/user.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [SideMenuComponent],
+  imports: [SideMenuComponent, CommonModule],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'tynbiz-general-app';
+  storeService = inject(StoreService);
+  public userRole = 'VISIT';
 
   ngOnInit(): void {
     initFlowbite();
+    this.storeService.user.subscribe((user) => {
+      if (user) this.userRole = user!.role;
+    });
   }
 }
